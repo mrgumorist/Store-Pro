e51d90cbdaed7321b878cbcdfa15d1afbed6024c
Andriy <mrgumorist@gmail.com>
2019-12-25 19:46:07 +0200

    e

---
+++ Store Pro/AdminPanel/Stores.xaml.cs
@@ -0,0 +1,55 @@
+﻿using Newtonsoft.Json;
+using Store_Pro.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Store_Pro.AdminPanel
+{
+    /// <summary>
+    /// Interaction logic for Stores.xaml
+    /// </summary>
+    public partial class Stores : Page
+    {
+        const string host = @"http://localhost:50471";
+        public Stores()
+        {
+            InitializeComponent();
+        }
+
+        private void Page_Loaded(object sender, RoutedEventArgs e)
+        {
+            const string WEBSERVICE_URL = host + @"/api/Apii/GetStores";
+            
+                var webRequest = System.Net.WebRequest.Create(WEBSERVICE_URL);
+                if (webRequest != null)
+                {
+                    webRequest.Method = "GET";
+                    webRequest.Timeout = 12000;
+                    webRequest.ContentType = "application/json";
+                    webRequest.Headers.Add("Authorization2", "Basic dchZ2VudDM6cGFdGVzC5zc3dvmQ=");
+                    using (System.IO.Stream s = webRequest.GetResponse().GetResponseStream())
+                    {
+                        using (System.IO.StreamReader sr = new System.IO.StreamReader(s))
+                        {
+                            var jsonResponse = sr.ReadToEnd();
+                            List<Store> response = JsonConvert.DeserializeObject<List<Store>>(jsonResponse) ;
+                            Dataa.ItemsSource = response;
+                        }
+                    }
+                }
+           
+        }
+    }
+}
--- Store Pro/AdminPanel/Адмін Панель.xaml.cs
+++ Store Pro/AdminPanel/Адмін Панель.xaml.cs
@@ -23,5 +23,36 @@ namespace Store_Pro.AdminPanel
         {
             InitializeComponent();
         }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            frame1.Source = new Uri("Golovna.xaml", UriKind.RelativeOrAbsolute);
+            frame2.Source = new Uri("Stores.xaml", UriKind.RelativeOrAbsolute);
+            frame3.Source = new Uri("Accounts.xaml", UriKind.RelativeOrAbsolute);
+            //frame1.Content = new Golovna();
+            //frame1.NavigationService.Navigate(new Golovna());
+
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            frame1.Visibility =Visibility.Visible;
+            frame2.Visibility = Visibility.Hidden;
+            frame3.Visibility = Visibility.Hidden;
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            frame1.Visibility = Visibility.Hidden;
+            frame2.Visibility = Visibility.Visible;
+            frame3.Visibility = Visibility.Hidden;
+        }
+
+        private void Button_Click_2(object sender, RoutedEventArgs e)
+        {
+            frame1.Visibility = Visibility.Hidden;
+            frame2.Visibility = Visibility.Hidden;
+            frame3.Visibility = Visibility.Visible;
+        }
     }
 }
